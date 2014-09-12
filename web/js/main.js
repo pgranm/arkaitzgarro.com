a1367a9efa64563fe1ffc4b8caa7d39e435e221d
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-09-12 16:40:48 +0200

    Add portfolio and publications

--- web/js/main.js
+++ web/js/main.js
@@ -8,4 +8,25 @@ $(document).ready(function() {
     });
     wow.init();
 
+    // ==================== MASONRY ==================== //
+    var $gallery = $('.gallery');
+    $gallery.masonry({
+        itemSelector: '.project'
+    });
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'')
+            && location.hostname == this.hostname)
+        {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: parseInt(target.offset().top) - $('.navbar').height()
+                }, 700);
+
+                return false;
+            }
+        }
+    });
 });
